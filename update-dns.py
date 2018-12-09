ac9819e398793cbf223ac984f042d57d6f3a1cb4
Elliot Holden <elliot@elliotmywebguy.com>
2018-12-09 00:22:04 +0000

    Changed relative file paths to absolute file paths

--- update-dns.py
+++ update-dns.py
@@ -10,7 +10,7 @@ import sys	# This is so you can use command line arguments
 my_hosted_zone_id = sys.argv[1] 
 
 # Pull down the current Route53 'A' record for your hosted zone, so you know what it was before you do the update
-os.system('aws route53 list-resource-record-sets --hosted-zone-id ' + my_hosted_zone_id + ' --query "ResourceRecordSets[?Type == \'A\']" >./output.txt 2>./errors.txt')
+os.system('aws route53 list-resource-record-sets --hosted-zone-id ' + my_hosted_zone_id + ' --query "ResourceRecordSets[?Type == \'A\']" >/home/elliot/update-dns/output.txt 2>/home/elliot/update-dns/errors.txt')
 
 # Get the current Public IPV4 address assigned to this instance
 current_ip_address = os.popen('curl http://169.254.169.254/latest/meta-data/public-ipv4 2>/dev/null').read()
@@ -19,13 +19,13 @@ current_ip_address = os.popen('curl http://169.254.169.254/latest/meta-data/publ
 # Open your template file and replace the ipv4 address with the one you got from the meta-data above. 
 # The 'with' statement usually encapsulates some setup/teardown open/close actions.
 # So basically the file is explicitly closed when you're done with it.
-with open('record-set-template.json') as myfile:
+with open('/home/elliot/update-dns/record-set-template.json') as myfile:
 	my_json = re.sub('[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+', current_ip_address, myfile.read())
 
 # Open a new file and write the template json that you modifed above
-f = open('new-record-set.json', 'w')
+f = open('/home/elliot/update-dns/new-record-set.json', 'w')
 f.write(my_json)
 f.close()
 
 # Update route 53 with the batch command using the new json file created above
-os.system('aws route53 change-resource-record-sets --hosted-zone-id Z2L2TMCJU3N9X3 --change-batch file://new-record-set.json &>dns-record-change.output')
+os.system('aws route53 change-resource-record-sets --hosted-zone-id Z2L2TMCJU3N9X3 --change-batch file:///home/elliot/update-dns/new-record-set.json &>/home/elliot/update-dns/dns-record-change.output')
